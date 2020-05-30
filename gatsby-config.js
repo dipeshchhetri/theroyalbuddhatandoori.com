9e421ed53481ac3c0215474727fc491f4a60b0f8
Sandesh Shrestha <mail@sandeshshrestha.com>
2020-05-30 23:29:10 +0200

    9

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
         name: `TheRoyalBuddhaTandoori`,
         short_name: `TheRoyalBuddhaTandoori`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffc32d`,
+        theme_color: `#ffc32d`,
         display: `minimal-ui`,
         icon: `src/images/logo.jpg`, // This path is relative to the root of the site.
       },
